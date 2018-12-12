66522748dccea47877807ea9e23bb14f9c4de943
darklion <kangdadada>
2018-12-12 22:10:30 +0900

    Add scv -> sendTransaction string

--- data/csv2set.py
+++ data/csv2set.py
@@ -3,7 +3,10 @@ import pandas as pd
 categories = ['game', 'productivity', 'social', 'wallpaper', 'weather']
 companies = dict()
 def _format(companyname, cate, row):
-	return 'set("' + companyname + '", "' + row['AppName'] + '", "' + cate + '", "' + row['Developer ID'] + '", "' + row['Source'] + '", "' + row['Sink'] + '");\n'
+	getdatastr = 'var getdata = contract.set.getData("' + companyname + '", "' + row['AppName'] + '", "' + cate + '", "' + row['Developer ID'] + '", "' + row['Source'] + '", "' + row['Sink'] + '");\n'
+	transactionstr = "var reciept = web3.eth.sendTransaction({ from: defaultAddr, to: contractAddr, data: getdata, gas: 7000000 });\n"
+	consolestr = "console.log(reciept);\n"
+	return getdatastr + transactionstr + consolestr
 
 def write_file(companies, cate):
 	df = pd.read_csv(cate+'.csv')
